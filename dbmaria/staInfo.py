2e6049bce57b3b99b8290d54acf146871e996068
Kumo Lam <16816538+Kumo-YZX@users.noreply.github.com>
2018-08-29 23:35:11 +0800

    add staInfo module

---
+++ dbmaria/staInfo.py
@@ -0,0 +1,63 @@
+#--#
+
+def loadModule(name, path):
+    import os, imp
+    return imp.load_source(name, os.path.join(os.path.dirname(__file__), path))
+
+loadModule('dbmaria', 'dbmaria.py')
+
+from dbmaria import dbBase
+import json
+
+class table(dbBase):
+
+    def __init__(self, configFile = 'dbconfig.json'):
+        dbBase.__init__(self, 'staInfo', configFile)
+
+    def create(self, definitionFile='staInfo_definition.json'):
+        with open(definitionFile) as fi:
+            self.createTable(json.load(fi))
+
+    def updateAll(self):
+        import urllib2
+        siteUrl = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js'
+        rawData = urllib2.urlopen(siteUrl, timeout=5).read()
+        rawData = rawData[21:-2]
+        allStation = rawData.split('@')
+        for everyStation in allStation:
+            infoSplit = everyStation.split('|')
+            self.insertData({"staCn": infoSplit[1].encode("hex"), "staTele": infoSplit[2], "staPy": infoSplit[4], "staNum":int(infoSplit[5])})
+        return 1
+
+    def delete(self, key='', value=''):
+        if key == '':
+            self.deleteData()
+        else:
+            self.deleteData([{key:{"judge":"=", "value":value}}])
+
+        return 1
+
+    def search(self, key='', value=''):
+        if key == '':
+            res = self.queryData()
+        else:
+            res = self.queryData([], [{key:{"judge":"=", "value":value}}])
+        
+        if len(res):
+            return 1, res
+        else:
+            return 0, []
+
+    def verify(self, key, value):
+        res = self.verifyExistence([{key:{"judge":"=", "value":value}}])
+        return res
+
+
+def initilize():
+    tbObj = table()
+    tbObj.create()
+    tbObj.delete()
+    tbObj.updateAll()
+
+if __name__ == "__main__":
+    initilize()
