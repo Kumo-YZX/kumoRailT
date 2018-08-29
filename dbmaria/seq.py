6eba2f36a0abfc5f0b5fe0dd03c904a97845e2f0
Kumo Lam <16816538+Kumo-YZX@users.noreply.github.com>
2018-08-30 00:01:21 +0800

    add seq module

---
+++ dbmaria/seq.py
@@ -0,0 +1,64 @@
+#---#
+
+import dbmaria
+import json
+
+class table(dbmaria.dbBase):
+
+    def __init__(self, configFile = 'dbconfig.json'):
+        dbmaria.dbBase.__init__(self, 'seq', configFile)
+
+    def create(self, definitionFile='seq_definition.json'):
+        with open(definitionFile) as fi:
+            self.createTable(json.load(fi))
+
+    def verify(self, key, value):
+        res = self.verifyExistence([{key:{"judge":"=", "value":value}}])
+        return res
+
+    def importAll(self, seqFile='seq_data.json'):
+        with open(seqFile) as fi:
+            seqList = json.load(fi)
+
+        for everySeq in seqList:
+            if self.verify('seqId', everySeq['sequence']):
+                print 'INFO: seq already exists'
+            else:
+                self.insertData({"seqId":everySeq['sequence'],
+                                "staff":everySeq['staffDep'],
+                                "depot":everySeq['vehicleDep'],
+                                "emuType":everySeq['emuType'].encode('utf8').encode('hex'),
+                                "remark":everySeq['remark'].encode('utf8').encode('hex')})
+
+        return 1
+
+    def delete(self, key='', value=''):
+        if key == '':
+            self.deleteData()
+        else:
+            self.deleteData([{key:{"judge":"=", "value":value}}])
+
+        return 1
+
+    def search(self, key='', value=''):
+        if key == '':
+            res = self.queryData()
+        else:
+            res = self.queryData([], [{key:{"judge":"=", "value":value}}])
+        
+        if len(res):
+            return 1, res
+        else:
+            return 0, []
+
+    # def update(self, key, value):
+    #     pass
+
+def initalize():
+    seqObj = table()
+    seqObj.create()
+    seqObj.importAll()
+
+if __name__ == "__main__":
+    initalize()
+
