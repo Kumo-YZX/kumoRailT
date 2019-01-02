72fbabaa30a1b6b9b4543331e9b2a5736c8a42f0
Kumo Lam <16816538+Kumo-YZX@users.noreply.github.com>
2019-01-02 18:22:32 +0800

    add words about errors

--- parse/chnword.py
+++ parse/chnword.py
@@ -2,8 +2,8 @@
 # Module Name: Chnword #
 # Function: Define commonly used chinese sentences in our reply. #
 # They need to be decoded in hex when using. #
-# Author: Kumo #
-# Last Edit: Dec/24/2018 #
+# Author: Kumo Lam(github.com/Kumo-YZX) #
+# Last Edit: Jan/01/2019 #
 #----------------------------------------------------------------#
 
 trainDoNotExist='E697A0E8B584E69699'
@@ -17,3 +17,15 @@ trainNo='E8BDA6E6ACA1'
 station='E8BDA6E7AB99'
 telecode='E794B5E68AA5E7A081'
 stationDoNotExist='E8AFA5E7AB99E4B88DE5AD98E59CA8'
+trainExistsButNoArrival='E8BDA6E6ACA1E5AD98E59CA8EFBC8CE4BD86E698AFE6B2A1E69C89E588B0E7AB' +\
+                        '99E695B0E68DAEE38082'
+
+depotQueryNotReady='E9858DE5B19EE69FA5E8AFA2E69A82E69CAAE4B88AE7BABFEFBC8CE695ACE8AFB7E8B' +\
+                   '085E8A7A3EFBC81'
+addtoLateMonitorNotReady='E6B7BBE58AA0E887B3E79B91E68EA7E69A82E69CAAE4B88AE7BABFEFBC8CE69' +\
+                         '5ACE8AFB7E8B085E8A7A3EFBC81'
+viewLateMonitorNotReady='E69FA5E79C8BE79B91E68EA7E58897E8A1A8E69A82E69CAAE4B88AE7BABFEFBC' +\
+                        '8CE695ACE8AFB7E8B085E8A7A3EFBC81'
+searchLateHistoryNotReady='E69FA5E79C8BE6999AE782B9E58E86E58FB2E69A82E69CAAE4B88AE7BABFEF' +\
+                          'BC8CE695ACE8AFB7E8B085E8A7A3EFBC81'
+conditionNotExist='E697A0E6B395E8AF86E588ABE79A84E591BDE4BBA4EFBC81'
